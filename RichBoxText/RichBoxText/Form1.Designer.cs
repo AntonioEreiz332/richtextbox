ba345218d229d1d270194d71f0d2636754a3d248
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2020-12-07 09:19:25 +0100

    Add files via upload

---
+++ RichBoxText/RichBoxText/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace RichBoxText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCitaj = new System.Windows.Forms.Button();
+            this.btnBrisi = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCitaj
+            // 
+            this.btnCitaj.Location = new System.Drawing.Point(13, 13);
+            this.btnCitaj.Name = "btnCitaj";
+            this.btnCitaj.Size = new System.Drawing.Size(75, 23);
+            this.btnCitaj.TabIndex = 0;
+            this.btnCitaj.Text = "Citaj";
+            this.btnCitaj.UseVisualStyleBackColor = true;
+            this.btnCitaj.Click += new System.EventHandler(this.btnCitaj_Click);
+            // 
+            // btnBrisi
+            // 
+            this.btnBrisi.Location = new System.Drawing.Point(329, 13);
+            this.btnBrisi.Name = "btnBrisi";
+            this.btnBrisi.Size = new System.Drawing.Size(75, 23);
+            this.btnBrisi.TabIndex = 1;
+            this.btnBrisi.Text = "Brisi";
+            this.btnBrisi.UseVisualStyleBackColor = true;
+            this.btnBrisi.Click += new System.EventHandler(this.btnBrisi_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(364, 174);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(13, 263);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(75, 23);
+            this.btnSpremi.TabIndex = 3;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            this.btnSpremi.Click += new System.EventHandler(this.btnSpremi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 303);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnBrisi);
+            this.Controls.Add(this.btnCitaj);
+            this.Name = "Form1";
+            this.Text = "Upotreba RichTextBox kontrole";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCitaj;
+        private System.Windows.Forms.Button btnBrisi;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnSpremi;
+    }
+}
+
